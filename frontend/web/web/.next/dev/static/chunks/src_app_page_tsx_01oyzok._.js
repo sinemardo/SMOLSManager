e72be8d43e8b05ed6a1838b9769587c4bb941529
GregoryFranco <gory66625@gmail.com>
2026-05-10 16:57:00 +0200

    fix: remove all .next cache folders, fix duplicate web directory, update gitignore

--- frontend/web/web/.next/dev/static/chunks/src_app_page_tsx_01oyzok._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_0ut0vtv._.js","static/chunks/src_app_page_tsx_0u-7s7d._.js"],
-    source: "dynamic"
-});
